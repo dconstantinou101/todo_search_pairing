c71e092a7f6a934bd52c4e0dde508c1a4ad65016
Debra Constantinou <debra.constantinou.tech@gmail.com>
2026-02-11 17:28:38 +0000

    Adds code to meet tests for format validation and edge cases

--- lib/age_checker.py
+++ lib/age_checker.py
@@ -2,26 +2,23 @@ from datetime import date, datetime
 
 def age_checker(date_of_birth):
 
-    dob = date.fromisoformat(date_of_birth)
+    if not isinstance(date_of_birth, str):
+        raise TypeError('Date must be in string format: YYYY-MM-DD')
+    try: 
+        dob = date.fromisoformat(date_of_birth)
+    except:
+        raise ValueError( 'Date must be in YYYY-MM-DD')
+    
     today = datetime.now().date() 
     
-    #rounds down to nearest integer
-    age = ((today - dob).days)//365
-    print(age)
+    
+    age = today.year - dob.year
+  
+    if (today.month, today.day) < (dob.month, dob.day):
+        age -= 1
+
     if age < 16:
         return f'Access is denied: your current age is {age} and you must be 16'
     
     return "Access granted!"
 
-""" def age_checker(date_of_birth):
-    dob = date.fromisoformat(date_of_birth)
-    today = datetime.now().date()
-
-    age = today.year - dob.year
-    print(age)
-    if (today.month, today.day) < (dob.month, dob.day):
-        age -= 1
-    if age < 16: 
-        return f"Access is denied: your current age is {age} and you must be 16"
-
-    return "Access granted!" """
--- tests/test_age_checker.py
+++ tests/test_age_checker.py
@@ -6,12 +6,23 @@ def test_access_granted_if_age_greater_than_15():
     result = age_checker("1960-10-21")
     assert result == "Access granted!"
 
-def test_access_is_denied_if_user_is_under_sixtee():
+def test_access_is_denied_if_user_is_under_sixteen():
     result = age_checker("2010-07-10")
     assert result == "Access is denied: your current age is 15 and you must be 16"
-####################################new
 
-""" def test_access_granted_if_age_is_16():
+
+def test_error_thrown_for_invalid_date_format():
+    with pytest.raises(ValueError) as error:
+        age_checker('01-02-2010')
+    assert str(error.value) == 'Date must be in YYYY-MM-DD'
+
+def test_error_thrown_when_not_passed_a_string():
+    with pytest.raises(TypeError) as error:
+        age_checker(15)
+    assert str(error.value) == 'Date must be in string format: YYYY-MM-DD'
+
+
+def test_access_granted_if_age_is_16():
     today = date.today()
     birth_date = date(today.year - 16, today.month, today.day)
     result = age_checker(birth_date.isoformat())
@@ -20,5 +31,6 @@ def test_access_is_denied_if_user_is_under_sixtee():
 def test_access_not_granted_if_one_day_before_birthday():
     today = date.today()
     birth_date = date(today.year - 16, today.month, today.day) + timedelta(days=1)
+    print(birth_date)
     result = age_checker(birth_date.isoformat())
-    assert result == "Access is denied: your current age is 15 and you must be 16" """
+    assert result == "Access is denied: your current age is 15 and you must be 16" 
